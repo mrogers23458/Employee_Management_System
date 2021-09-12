7c4a5948271ca43644c0458fd77c8bcd5dac80d7
Mike Rogers <mrogers23458@gmail.com>
2021-09-11 20:18:26 -0700

    created conditional statements in init function that will query employees_db for information based on user input

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const iq = require('inquirer')
+const mysql = require('mysql2')
+const pass = require('../../pass/pass')
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: pass,
+        database: 'employees_db'
+    },
+    console.log('Connected to the employees_db database')
+);
+
 const empDbQuery = function (){
     let test = ['one', 'two', 'three']
     return test
@@ -55,4 +68,28 @@ const updatePrompt = [{
     choices: empDbQuery
 }]
 
-iq.prompt(updatePrompt)
+const init = async() => {
+    const res = await iq.prompt(initPrompt)
+    console.log(initPrompt);
+
+    if (res.initAction === 'view all departments'){
+        console.log('view all departments')
+        db.query('SELECT * FROM department', function (err, results){
+            console.log(results)
+        })
+    }
+    if (res.initAction === 'view all roles'){
+        console.log('view all departments')
+        db.query('SELECT * FROM role', function (err, results){
+            console.log(results)
+        })
+    }
+    if (res.initAction === 'view all employees'){
+        console.log('view all departments')
+        db.query('SELECT * FROM employee', function (err, results){
+            console.log(results)
+        })
+    }
+}
+
+init();
